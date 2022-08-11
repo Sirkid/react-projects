fac71ca6cbdf91d281c68647ee072ee19970def7
Sirkid <lesterchee17@gmail.com>
2022-08-11 15:31:52 +0800

    init

--- 07-slider/setup/src/Alternative.js
+++ 07-slider/setup/src/Alternative.js
@@ -6,22 +6,47 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex)
-    }
-    if (index >  lastIndex) {
-      setIndex(0);
-    }
-  },[index,people])
+  const nextSlide = () => {
+      setIndex((oldIndex) => {
+          let index = oldIndex + 1
+          if (index > people.length - 1) {
+              index = 0
+          }
+          return index
+    })
+    }  
+    const prevSlide = () => {
+      setIndex((oldIndex) => {
+        let index = oldIndex - 1;
+        if (index < 0) {
+          index = people.length - 1
+        }
+        return index;
+      });
+    };  
+    
+//   useEffect(() => {
+//     const lastIndex = people.length - 1;
+//     if (index < 0) {
+//       setIndex(lastIndex);
+//     }
+//     if (index > lastIndex) {
+//       setIndex(0);
+//     }
+//   }, [index, people]);
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1)
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
     }, 3000);
     return () => clearInterval(slider);
-  },[index])
+  }, [index]);
 
   return (
     <section className='section'>
@@ -54,10 +79,10 @@ function App() {
             </article>
           );
         })}
-        <button className='prev' onClick={() => setIndex(index - 1)}>
+        <button className='prev' onClick={prevSlide}>
           <FiChevronLeft />
         </button>
-        <button className='next' onClick={() => setIndex(index + 1)}>
+        <button className='next' onClick={nextSlide}>
           <FiChevronRight />
         </button>
       </div>
